eacd549e3c3ca052abc4c443e43cf23512580fdb
Diana Tuz <dianatuz12@gmail.com>
2023-02-11 01:28:35 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready((function () {
+    $('.nav__burger').click(function () {
+        $('.nav__burger, .show').toggleClass('active');
+        $('body').toggleClass('lock')
+    })
+}))
